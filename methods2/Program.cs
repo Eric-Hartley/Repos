b1c0d8d27a8a4729d8738eb1e195831b6fec8f7c
Eric Hartley <spiffydude0@gmail.com>
2024-08-01 21:57:36 -0500

    trying to get a range for a list, then loop through that list for set range

---
+++ methods2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MyCompany.MyProduct.System
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Capacity of List: ");
+            int range = Int32.Parse(Console.ReadLine());
+
+            List<int> numbers = new List<int>(range);
+
+            for (int i = 0; i < range; i++)
+            {
+                numbers.Add(i);
+            }
+
+            
+
+            foreach (int number in numbers)
+            {
+                int count = 1;
+                Console.WriteLine($"Number {count}: ");
+                Int32.Parse(Console.ReadLine());
+                numbers.Add(number);
+                
+                count++;
+                
+            }
+
+            Console.WriteLine($"Capacity: {numbers.Capacity}");
+            
+        }
+    }
+
+    
+}
